83d8c91d90a6a89219d0aee1df95cc57f9c9ce4a
Logan Laughery <lockre35@gmail.com>
2018-04-09 21:29:29 -0500

    Minor updates to pdf styling

---
+++ assets/default.js
@@ -0,0 +1,21 @@
+exports.content = 
+`
+<html>
+  <head>
+    <style>
+      html {
+        width: 100%;
+      }
+
+      img {
+        width: 500px;
+        margin: 0 auto;
+      }
+    </style>
+  </head>
+  <body style="width: 100%">
+    <img src="holder"/>
+    <img src="holder"/>
+  </body>
+</html>
+`;
--- src/generatePdf.js
+++ src/generatePdf.js
@@ -1,7 +1,10 @@
 const htmlPdf = require('html-pdf');
+const defaultHtml = require('../assets/default.js');
+const htmlText = defaultHtml.content;
 
-const htmlText = '<html><body><img style="width: 200px" src="holder"/></body></html>';
+var options = { format: 'Letter' };
+
 exports.generatePdf = (base64Stream, callback) => {
-  htmlPdf.create(htmlText.replace(/holder/g, base64Stream))
+  htmlPdf.create(htmlText.replace(/holder/g, base64Stream), options)
     .toBuffer(callback);
 };
