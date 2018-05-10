a987df0607dd7dfd163139c0939cfcb9405e1b8f
Logan Laughery <lockre35@gmail.com>
2018-05-09 21:43:46 -0500

    Cors working on cloud function

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 const pdfGenerator = require('./src/generatePdf.js');
+const cors = require("cors");
+
 /**
  * HTTP Cloud Function.
  * 
@@ -7,21 +9,23 @@ const pdfGenerator = require('./src/generatePdf.js');
  * @param {Object} res Cloud Function response context.
  **/
 exports.getPdf = (req, res) => {
-  if (req.method === 'OPTIONS') {
+  /**if (req.method === 'OPTIONS') {
     res.set('Access-Control-Allow-Origin', '*')
       .set('Access-Control-Allow-Origin', 'GET, POST')
       .set('Access-Control-Expose-Headers', 'Content-Disposition')
       .status(200);
     return;
-  }
-
-  pdfGenerator.generatePdf(req.body.image, (err, data) => {
-    //res.send(data);
-    res.writeHead(200, {
-      'Content-Type': 'application/pdf',
-      'Content-Disposition': 'attachment; filename=some_file.pdf',
-      'Content-Length': data.length
+  }**/
+  var corsFn = cors({ origin: true, exposedHeaders: 'Content-Disposition' });
+  corsFn(req, res, function() {
+    pdfGenerator.generatePdf(req.body.image, (err, data) => {
+      //res.send(data);
+      res.writeHead(200, {
+        'Content-Type': 'application/pdf',
+        'Content-Disposition': 'attachment; filename=some_file.pdf',
+        'Content-Length': data.length
+      });
+      res.end(data);
     });
-    res.end(data);
   });
 };
---
+++ readme.md
@@ -0,0 +1,6 @@
+## Local Debuggin
+-Start server with 'npm test'
+
+## Deployment Steps
+-Create zip folder containing contents of this repo
+-Upload to cloud functions
